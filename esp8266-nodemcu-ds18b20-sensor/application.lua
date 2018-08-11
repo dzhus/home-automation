044fbabb9a0a64c1441b1a63bc089c9a9b394080
Dmitry Dzhus <dima@dzhus.org>
2018-08-11 21:03:40 +0100

    fix: Don't deep sleep as it doesn't work with my modules
    Simply turn off Wi-Fi until the end of the sleep cycle

--- esp8266-nodemcu-ds18b20-sensor/application.lua
+++ esp8266-nodemcu-ds18b20-sensor/application.lua
@@ -1,26 +1,50 @@
 local module = {}
 
-local function push_temp(client)
-  ds18b20.read(
-    function(index, rom, res, temp, temp_dec, par)
-      client:publish(
-        config.TOPIC, string.format("%d.%02d", temp, temp_dec), 0, 0,
+local function push_temp(msg)
+  m = mqtt.Client(config.ID, 120)
+  m:connect(config.MQTT_HOST, config.MQTT_PORT, 0, 0,
+    function (client)
+      print("Publishing to MQTT")
+      client:publish(config.TOPIC, msg, 0, 0,
         function ()
-          node.dsleep(config.SLEEP_S * 1000 * 1000)
-        end)
-    end, {})
+          print("Sleeping")
+          wifi.setmode(wifi.NULLMODE)
+          tmr.create():alarm(config.SLEEP_S * 1000, tmr.ALARM_SINGLE,
+                             function () node.restart() end)
+        end
+      )
+    end
+  )
 end
 
-local function mqtt_start()
-  m = mqtt.Client(config.ID, 120)
-  m:connect(config.MQTT_HOST, config.MQTT_PORT, 0, 0, function (client)
-              push_temp(client)
-  end)
+local function prepare_wifi(msg)
+  scfg = {}
+  scfg.ssid = config.SSID
+  scfg.pwd = config.PWD
+  scfg.auto = false
+  scfg.got_ip_cb = function()
+    print("WIFI_STA_GOT_IP")
+    push_temp(msg)
+  end
+
+  wifi.setmode(wifi.STATION)
+  wifi.sta.config(scfg)
+  wifi.sta.connect()
 end
 
 function module.start()
+  print("Application starting")
   ds18b20.setup(4) -- ESP-01 DS18B20 shield
-  mqtt_start()
+  ds18b20.setting({}, 9)
+  tmr.create():alarm(1 * 1000, tmr.ALARM_SINGLE,
+    function ()
+      ds18b20.read(
+        function(index, rom, res, temp, temp_dec, par)
+          prepare_wifi(string.format("%d.%04d", temp2, temp_dec2))
+        end
+      )
+    end
+  )
 end
 
 return module
--- esp8266-nodemcu-ds18b20-sensor/setup.lua
+++ esp8266-nodemcu-ds18b20-sensor/setup.lua
@@ -1,30 +1,7 @@
 local module = {}
 
-local function wifi_wait_ip_and_start()
-  if wifi.sta.getip() == nil then
-    print("Waiting for IP")
-  else
-    tmr.stop(1)
-    print("\n====================================")
-    print("ESP8266 mode is: " .. wifi.getmode())
-    print("MAC address is: " .. wifi.ap.getmac())
-    print("IP is " .. wifi.sta.getip())
-    print("====================================")
-    app.start()
-  end
-end
-
-local function wifi_start()
-  scfg = {}
-  scfg.ssid = config.SSID
-  scfg.pwd = config.PWD
-  wifi.sta.config(scfg)
-  wifi.sta.connect()
-  tmr.alarm(1, 2500, 1, wifi_wait_ip_and_start)
-end
-
 function module.start()
-  wifi_start()
+  app.start()
 end
 
 return module
